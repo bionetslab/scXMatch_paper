f88c010cc97020b23b5207ad99e6713ffc1a1a60
Anna Moeller <iwbn007h@mailhub.rrze.uni-erlangen.de>
2026-05-19 13:22:33 +0200

    edist results

--- src/benchmarking/edist/var_e_distances_on_pertpy_data.py
+++ src/benchmarking/edist/var_e_distances_on_pertpy_data.py
@@ -68,7 +68,7 @@ def get_e_distance_log(adata, group_by, reference, subsampling):
 def main(dataset_path):
     names = sorted([f for f in os.listdir(dataset_path) if f.endswith("hdf5")])
     files = [os.path.join(dataset_path, f) for f in names]
-    subsampling = False
+    subsampling = True
     print("files", files)
     
     for f in files:
@@ -78,6 +78,8 @@ def main(dataset_path):
             p = os.path.basename(f).split(".")[0] + "_unbalanced"
         p = os.path.join("/home/woody/iwbn/iwbn007h/scXMatch_paper/evaluation_results/1_14_var_edistance", f"{p}_e_distance_results.csv")
         
+        if os.path.exists(p):
+            continue
         adata = ad.read_h5ad(f)
         if "mcfarland" in f:
             group_by = "pert_time"
@@ -105,7 +107,6 @@ def main(dataset_path):
         adata = ad.read_h5ad(f)
         adata = adata[adata.obs[group_by].notna()].copy()
         adata.obs[group_by] = adata.obs[group_by].astype(str)
-        
         groups =  adata.obs[group_by].unique()
         all_results = []
         for test_group in groups:
@@ -115,7 +116,7 @@ def main(dataset_path):
                 subset_1 = adata[( (adata.obs[group_by] == test_group) & (adata.obs[group_by_split] == 1) ), :].obs.index
                 for group_by_split_reference in ["split_10", "split_30", "split_50"]:    
                     subset_2 = adata[( (adata.obs[group_by] == reference) & (adata.obs[group_by_split_reference] == 1) ), :].obs.index
-                    results = get_e_distance_log(adata[list(subset_1) + list(subset_2)], group_by=group_by, reference=reference, test_group=test_group)   
+                    results = get_e_distance_log(adata[list(subset_1) + list(subset_2)], group_by=group_by, reference=reference, subsampling=subsampling)   
                     results["group_by_split"] = group_by_split
                     results["group_by_split_reference"] = group_by_split_reference
                     results["test_group"] = test_group
