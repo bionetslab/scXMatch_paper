ca43e77addced540d729f3af635d6b544f7c4650
Anna Moeller <iwbn007h@mailhub.rrze.uni-erlangen.de>
2026-06-12 08:48:26 +0200

    gaussian runs hpc

--- src/runtime_on_random.py
+++ src/runtime_on_random.py
@@ -1,7 +1,6 @@
 import anndata as ad
 from time import time
 import pandas as pd
-from scxmatch import test
 import scxmatch as xm
 import glob
 import os
@@ -9,79 +8,20 @@ import fcntl
 import sys
 import resource
 from itertools import product
+from runtime_on_splatter import get_peak_ram_gb, run_matching, write_row, process_file
+
 
 splatter_dir = "/home/woody/iwbn/iwbn007h/xm_gaussian_data/random_normal_sims"
-out_file = "/home/woody/iwbn/iwbn007h/scXMatch_paper/evaluation_results/0_1_splatter_runtime/random_results.csv"
-
-def get_peak_ram_gb():
-    """
-    Returns peak RAM usage in GiB.
-    Linux ru_maxrss is in KB.
-    Includes child processes.
-    """
-    self_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    child_kb = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss
-
-    total_gb = (self_kb + child_kb) / 1024 / 1024
-    return total_gb
-
-
-def run_matching(adata, k, metric="sqeuclidean"):
-
-    t0 = time()
-    xm._match._kNN(adata, k, metric)
-    t1 = time()
-    G = xm._match._construct_graph_via_kNN(adata)
-    matching = xm._match._match(G, len(adata))
-    t2 = time()
-    num_edges = len(G.get_edges())
-    
-    result = dict()
-    result["t_NN [s]"] = t1 - t0
-    result["t_matching [s]"] = t2 - t1
-    result["peak_ram_gb"] = get_peak_ram_gb()
-    result["num_edges"] = num_edges
-    result["metric"] = metric
-    return result
-
-
-def write_row(result_dict):
-    df = pd.DataFrame([result_dict])
-
-    file_exists = os.path.isfile(out_file)
-
-    with open(out_file, "a") as f:
-        # lock file to avoid race conditions
-        fcntl.flock(f, fcntl.LOCK_EX)
-        df.to_csv(f, header=not file_exists, index=False)
-        fcntl.flock(f, fcntl.LOCK_UN)
-
-
-def process_file(file, k, idx):
-    adata = ad.read_h5ad(file)
-    n_obs, n_var = adata.shape
-
-    result = run_matching(adata, k, "sqeuclidean")
-
-    result["n_obs"] = n_obs
-    result["n_var"] = n_var
-    result["file"] = os.path.basename(file)
-    result["k"] = k
-    result["slurm_job_id"] = os.environ.get("SLURM_JOB_ID")
-    result["slurm_array_idx"] =  idx
-    write_row(result)
+out_file = "/home/woody/iwbn/iwbn007h/scXMatch_paper/evaluation_results/0_1_splatter_runtime/random_results_with_s.csv"
 
 
 if __name__ == "__main__":
-
     files = sorted(glob.glob(os.path.join(splatter_dir, "*.h5ad")))
-    ks = [10, 100, 5000] #[10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
-    combinations = list(product(files, ks))
-    
+    ks = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+    combinations = list(product(files, ks)) 
+
     idx = int(sys.argv[1])
     file, k = combinations[idx]
     print("FILE:", file, file=sys.stderr, flush=True)
     print("K:", k, file=sys.stderr, flush=True)  
-    print("FILE:", file, file=sys.stdout, flush=True)
-    print("K:", k, file=sys.stdout, flush=True)  
-    process_file(file, k, idx)
+    process_file(file, k, idx, out_file)
--- src/runtime_on_splatter.py
+++ src/runtime_on_splatter.py
@@ -11,7 +11,6 @@ import resource
 from itertools import product
 
 splatter_dir = "/home/woody/iwbn/iwbn007h/xm_splatter_data_recovered/splatter_sims"
-out_file = "/home/woody/iwbn/iwbn007h/scXMatch_paper/evaluation_results/0_1_splatter_runtime/splatter_results.csv"
 
 def get_peak_ram_gb():
     """
@@ -26,27 +25,6 @@ def get_peak_ram_gb():
     return total_gb
 
 
-def run_test(adata, k, metric):
-
-    start_time = time()
-
-    result = test(
-        adata,
-        k=k,
-        metric=metric,
-        group_by="Group",
-        reference="Group1",
-        test_group="Group2"
-    )
-
-    end_time = time()
-
-    result["runtime_sec"] = end_time - start_time
-    result["peak_ram_gb"] = get_peak_ram_gb()
-
-    return result
-
-
 def run_matching(adata, k, metric):
 
     t0 = time()
@@ -57,16 +35,19 @@ def run_matching(adata, k, metric):
     t2 = time()
     num_edges = len(G.get_edges())
     
+    relative_support = len(matching) * 2 / len(adata)
+    
     result = dict()
     result["t_NN [s]"] = t1 - t0
     result["t_matching [s]"] = t2 - t1
     result["peak_ram_gb"] = get_peak_ram_gb()
     result["num_edges"] = num_edges
     result["metric"] = metric
+    result["relative_support"] = relative_support
     return result
 
 
-def write_row(result_dict):
+def write_row(result_dict, out_file):
     df = pd.DataFrame([result_dict])
 
     file_exists = os.path.isfile(out_file)
@@ -74,15 +55,14 @@ def write_row(result_dict):
     with open(out_file, "a") as f:
         # lock file to avoid race conditions
         fcntl.flock(f, fcntl.LOCK_EX)
-
         df.to_csv(f, header=not file_exists, index=False)
-
         fcntl.flock(f, fcntl.LOCK_UN)
 
 
-def process_file(file, k, idx):
+def process_file(file, k, idx, out_file):
     adata = ad.read_h5ad(file)
-    adata.X = adata.layers["X"]
+    if "X" in adata.layers:
+        adata.X = adata.layers["X"]
     n_obs, n_var = adata.shape
 
     result = run_matching(adata, k, "sqeuclidean")
@@ -94,19 +74,21 @@ def process_file(file, k, idx):
     result["slurm_job_id"] = os.environ.get("SLURM_JOB_ID")
     result["slurm_array_idx"] =  idx
 
-    write_row(result)
+    write_row(result, out_file)
 
 
 if __name__ == "__main__":
-
+    out_file = "/home/woody/iwbn/iwbn007h/scXMatch_paper/evaluation_results/0_1_splatter_runtime/splatter_results_with_s.csv"
     files = sorted(glob.glob(os.path.join(splatter_dir, "*.h5ad")))
     ks = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
     combinations = list(product(files, ks))
     
     idx = int(sys.argv[1])
     file, k = combinations[idx]
+    n_obs = int(os.path.basename(file).split("sim_N")[1].split("_")[0])
+    if k >= n_obs:
+        print(f"Skipping file {file} with k={k} >= n_obs={n_obs}", file=sys.stderr, flush=True)
+        sys.exit(0)
     print("FILE:", file, file=sys.stderr, flush=True)
     print("K:", k, file=sys.stderr, flush=True)  
-    print("FILE:", file, file=sys.stdout, flush=True)
-    print("K:", k, file=sys.stdout, flush=True)  
-    process_file(file, k, idx)
+    process_file(file, k, idx, out_file)
